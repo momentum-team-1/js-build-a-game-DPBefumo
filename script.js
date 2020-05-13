c214f5c9de7835618a232fa16a105a5e806bc6b7
Danny Befumo <DPBefumo@gmail.com>
2020-05-13 13:21:18 -0400

    Still having issues with bullet and enemies, but enemy blocks are on screen

--- script.js
+++ script.js
@@ -4,13 +4,22 @@ class Game {
         let context = canvas.getContext('2d')
         let gameSize = { x: canvas.width, y: canvas.height }
         this.player = new Player(gameSize)
+        this.ememy = new Enemy(game, center)// what argument should be here?
         let animate = () => {
             this.update()
-            this.drawPlayer(context, gameSize) //will this hold true or need to be altered?
-            // this.drawBullet()
+            this.drawPlayer(context, gameSize) 
+            this.drawEnemy(context)// what argument should be here?
             requestAnimationFrame(animate)
         }
         animate()
+    }
+
+    update() {
+        //use this to contain all objects
+        this.player.update()
+        // this.bullet.update()
+        this.enemy.update() //Uncaught TypeError: Cannot read property 'update' of undefined keeps popping up
+
     }
 
     drawPlayer (context, gameSize) {
@@ -22,28 +31,23 @@ class Game {
         let playerHeight = this.player.size.y
         context.fillRect(startingXPosition, startingYPosition, playerWidth, playerHeight)
     }
-    
-    // drawBullet () {
-    
-    // }
 
-    // drawBomb () {
-
-    // }
-
-    // drawEnemy (context) {
-    //     context.fillRect(startingXPosition, startingYPosition, playerWidth, playerHeight)
-    // }
-
-    // drawBonus () {
-
-    // }
-    update() {
-        //use this to contain all objects
-        this.player.update()
-        // this.bullet.update()
-        // this.enemy.update()
+    drawEnemy (context) { //make them random starting positons
+        context.fillystyle = 'red'
+        context.fillRect(30, -10, 25, 25)
+        context.fillRect(80, -10, 25, 25)
+        context.fillRect(130, -10, 25, 25)
+        context.fillRect(180, -10, 25, 25)
+        context.fillRect(230, -10, 25, 25)
     }
+
+    //draw bullet
+    //draw bonus
+    //draw bomb
+
+    //need to take score
+    //need to count down lives
+    //need to have an end to game when out of lives
 }
 
 class Player {
@@ -52,22 +56,20 @@ class Player {
         this.center = { x:gameSize.x / 2, y: gameSize.y - this.size.y * 1 }
         this.keyboarder = Keyboarder
     }
-    update () {
+    update () {//how do I make the player not go off screen?
         if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
             this.center.x += 2
         } else if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
             this.center.x -= 2
-        }
-        
+        } //how to add in the bullet?
         if (this.keyboarder.isDown(this.keyboarder.KEYS.UP)) {
             let bullet = new Bullet ({ x: this.center.x, y: this.center.y - this.size.y - 10},
             { x: 0, y: -5})
-            // this.game.addBody(bullet)
         } 
     }
 }
 
-class Bullet {
+class Bullet { //not sure if this even works
     constructor (center, velocity) {
         this.center = center
         this.size = { x: 3, y: 3 }
@@ -79,31 +81,31 @@ class Bullet {
     }
 }
 
-// class Bomb {
-//     constructor () {
-
-//     }
-// }
-
-class Enemy {
-    constructor (game) {
+class Enemy { //need to make the enemies move!
+    constructor (game, center) { // is this even the correct argument to bring in?
         this.game = game
-        this.size = { x: 30, y: 30 }
+        this.center = center //may be unneeded since the update is running off of gravity speed
+        // this.size = { x: 30, y: 30 }
         this.patrolY = 0
-        this.speedY = 0.4
+        this.speedX = 0
+        this.speedY = 0
+        this.gravity = 0.05
+        this.gravitySpeed = 0
     }
+      //want it to disappear if it passes the bottom
+      //need it to replicate itself in random places
     update () {
-        if (this.patrolY < 0) {
-            //want it to disappear if it passes the bottom
-            //need it to replicate itself in random places
+        if (this.patrolY > 0) {
+            console.log('is this working?')
+            this.gravitySpeed += this.gravity
+            this.x += this.speedX
+            this.y += this.speedY + this.gravitySpeed
         }
     }
 }
 
-// class Bonus {
-//     constructor () {
+// create a bomb class
+// create a bonus class
 
-//     }
-// }
 
 new Game ()
